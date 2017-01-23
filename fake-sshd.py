251cad817371ba3fbcc7622bef2a92f0c115d424
benoitmaudet <benoit.maudet@gmail.com>
2017-01-23 16:01:06 +0100

    Threading, formatting

--- fake-sshd.py
+++ fake-sshd.py
@@ -4,7 +4,7 @@
 # description	    :This script is a fake ssh server logging authentication (login/password or public key) requests.
 # author		    :Benoit MAUDET
 # date			    :20170101
-# version		    :0.1
+# version		    :0.2
 # usage			    :python fake-sshd.py
 # notes			    :requires: apt install python-setuptools python-pip python-dev gcc & pip install paramiko python-gssapi
 # notes			    :to generate rsa_ssh: ssh-keygen rsa
@@ -14,13 +14,15 @@ import threading
 import traceback
 import paramiko
 
-paramiko.util.log_to_file('ssh_server.log')
-host_key = paramiko.RSAKey(filename='rsa_ssh')
+bind_port = 22
 fake_version = 'SSH-2.0-OpenSSH_3.7.1p2'
+host_key = paramiko.RSAKey(filename='rsa_ssh')
+
 
 class FakeSSHServer(paramiko.ServerInterface):
-    def __init__(self):
-        self.event = threading.Event()
+
+    def __init__(self, address):
+        self.address = address
 
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
@@ -28,13 +30,17 @@ class FakeSSHServer(paramiko.ServerInterface):
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def check_auth_password(self, username, password):
-        print "\t%s %s" % (username, password)
-        auth_logFile.write("\t%s %s\n" % (username, password))
+        print("%s %s %s" % (self.address, username, password))
+        auth_logfile = open("ssh_auth.log", 'a')
+        auth_logfile.write("%s %s %s\n" % (self.address, username, password))
+        auth_logfile.close()
         return paramiko.AUTH_FAILED
 
     def check_auth_publickey(self, username, key):
-        print "\t%s %s %s" % (username, key.get_name(), key.get_base64())
-        auth_logFile.write("\t%s %s %s\n" % (username, key.get_name(), key.get_base64()))
+        print("%s %s %s %s" % (self.address, username, key.get_name(), key.get_base64()))
+        auth_logfile = open("ssh_auth.log", 'a')
+        auth_logfile.write("%s %s %s %s\n" % (self.address, username, key.get_name(), key.get_base64()))
+        auth_logfile.close()
         return paramiko.AUTH_FAILED
 
     def get_allowed_auths(self, username):
@@ -49,60 +55,46 @@ class FakeSSHServer(paramiko.ServerInterface):
         return False
 
 
-while True:
-    auth_logFile = open("ssh_auth.log", 'a')
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('', 22))
-    except Exception as e:
-        traceback.print_exc()
-        auth_logFile.close()
-        continue
+class ThreadedFakeSSHServer:
+    def __init__(self):
+        self.event = threading.Event()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(('', bind_port))
 
-    try:
-        sock.listen(100)
-        client, addr = sock.accept()
-        print("%s" % addr[0])
-        auth_logFile.write("%s\n" % addr[0])
-    except Exception as e:
-        traceback.print_exc()
-        auth_logFile.close()
-        continue
+    def listen(self):
+        self.sock.listen(100)
+        while True:
+            try:
+                client, address = self.sock.accept()
+                client.settimeout(60)
+                threading.Thread(target=self.listen_to_client, args=(client, address)).start()
+            except Exception as e:
+                traceback.print_exc()
+                continue
 
-    try:
+    def listen_to_client(self, client, address):
         transport = paramiko.Transport(client)
-        transport.set_gss_host(socket.getfqdn(""))
         try:
             transport.load_server_moduli()
         except:
+            transport.close()
             raise
         transport.add_server_key(host_key)
-        server = FakeSSHServer()
-        try:
-            transport.local_version = fake_version
-            transport.start_server(server=server)
-        except paramiko.SSHException:
-            auth_logFile.close()
-            continue
+        server = FakeSSHServer(address[0])
+        transport.local_version = fake_version
+        transport.start_server(server=server)
+
         # wait for auth
         chan = transport.accept(20)
 
-        auth_logFile.close()
-
         if chan is None:
             transport.close()
-            continue
-        server.event.wait(2)
-        if not server.event.is_set():
+        else:
+            server.event.wait(2)
+            if not server.event.is_set():
+                transport.close()
+            chan.close()
             transport.close()
-            continue
-        chan.close()
-        transport.close()
 
-    except Exception as e:
-        traceback.print_exc()
-        try:
-            transport.close()
-        except:
-            pass
+ThreadedFakeSSHServer().listen()
